83ff1d71f626aa9691ad2ad61200412937d7ec46
kimsujeong <101383852+ttochino@users.noreply.github.com>
2022-09-19 21:16:33 +0900

    Delete src directory

--- src/img.js
+++
@@ -1,8 +0,0 @@
-const images = ["01.jpg", "02.jpg", "03.jpg"]
-
-const imgBox = document.querySelector(".imgBox")
-const chosenImage = images[Math.floor(Math.random() * images.length)];
-
-const bgImage = document.createElement("img");
-bgImage.src = `src/img/${chosenImage}`;
-imgBox.appendChild(bgImage);
